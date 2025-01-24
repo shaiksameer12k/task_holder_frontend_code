374be103c920fea251cf0b122834217850d3b1d5
sameer.s@dcoretech.in <sameer.s@dcoretech.in>
2025-01-24 16:17:13 +0530

    added / in route

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,70 +1,3 @@
-// import axios from "axios";
-
-// import { ApiHeaders } from "./apiHeader";
-
-// import { useCallback, useState } from "react";
-// import { message } from "antd";
-// import { env } from "../utils/constant";
-
-// export const useApiCalls = () => {
-//   const [loadingStates, setLoadingStates] = useState({});
-
-//   const setLoading = useCallback((key, value) => {
-//     setLoadingStates((prev) => ({ ...prev, [key]: value }));
-//   }, []);
-
-//   async function ApiCalls(
-//     functionName = "dummy",
-//     type = "get",
-//     route,
-//     params,
-//     token
-//   ) {
-//     console.log("ApiCalls", functionName, type, route, params, token);
-//     try {
-//       setLoading(functionName, true);
-//       let url = `${env.VITE_API_URL}${route}`;
-//       let result;
-
-//       type != "get"
-//         ? (result = await axios[type](
-//             url,
-//             (data = params),
-//             ApiHeaders(type, token)
-//           ))
-//         : (result = await axios[type](url, ApiHeaders(type, token)));
-
-//       result = await result.data;
-
-//       console.log("result***", result);
-
-//       if (result) {
-//         let { statusCode, messageData, isToastMessage, data = [] } = result;
-//         console.log("statusCode", statusCode, isToastMessage, messageData);
-//         isToastMessage == true &&
-//           message[statusCode == 200 ? "success" : "error"](messageData);
-//       }
-//       return result;
-//     } catch (error) {
-//       console.log(`${route} : ${error}`);
-//     } finally {
-//       setLoading(functionName, false);
-//     }
-//   }
-
-//   return { ApiCalls, loadingStates };
-// };
-
-// /*
-// result schema from api
-// [{
-//  statusCode:0 / 1, statusCode 1 success, statusCode 0 error
-//  message:"",
-//  isToastAvalable:0 / 1,  toast 1 show messae in toast , toast 0 not to show
-//  data:[],
-// }]
-//  */
-
 import axios from "axios";
 import { ApiHeaders } from "./apiHeader";
 import { useCallback, useState } from "react";
@@ -96,7 +29,7 @@ export const useApiCalls = () => {
     );
     try {
       setLoading(functionName, true);
-      let url = `${env.VITE_API_URL}${route}`;
+      let url = `${env.VITE_API_URL}${route}/`;
       let result;
 
       let config = {
